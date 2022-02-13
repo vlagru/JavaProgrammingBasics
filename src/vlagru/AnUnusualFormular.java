f3a17a9cc2aeb990390383e885df1911e46e1622
vlagru <v.grulich@seznam.cz>
2022-02-13 20:55:47 +0100

    Add new program

---
+++ src/vlagru/AnUnusualFormular.java
@@ -0,0 +1,43 @@
+package vlagru;
+
+import java.util.Scanner;
+
+public class AnUnusualFormular {
+
+    public static void main(String[] args) {
+
+        // How old are you? And do you like dogs or cats? Be careful of what you're gonna answer! :]
+
+        System.out.println("Kolik ti je roku?");
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Mas radsi kocky, nebo psy?");
+        Scanner scanner2 = new Scanner(System.in);
+        String jmeno = scanner.nextLine();
+
+        if (input >= 18 && !"psy".equals(jmeno)) // nebere desetinna cisla
+            /* >
+             * <
+             * =!
+             * <=
+             * >=
+             * &&
+             * ||
+             * !
+             * && and || nejdou dohromady
+             */
+        {
+            System.out.println("Muzes si podat prihlasku.");
+
+        } else
+        {
+            System.out.println("Je nam lito, ale nelze si podat prihlasku.");
+
+        }
+    }
+
+
+
+
+
---
+++ src/vlagru/CzechProverbs.java
@@ -0,0 +1,42 @@
+package vlagru;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CzechProverbs {
+
+    public static void main(String[] args) {
+
+        // A program in Czech generating Czech famous proverbs on the basis of what you are going to type in the log (1-5).
+
+        System.out.println("Napis cele cislo od 1 do 5: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        // int input = Integer.parseInt(scanner.nextLine()); u int nebere cela cisla
+
+        switch(input) {
+            case "1":
+                System.out.println("Sul nad zlato.");
+                break;// kdyz vypustim break a citat pro '1' a '2' plati to same, co plati pro '2'
+            case "2":
+                System.out.println("Kdo chce psa biti, hul si vzdycky najde.");
+                break;
+            case "3":
+                System.out.println("Kdo jinemu...");
+                break;
+            case "4":
+                System.out.println("Devatero remesel...");
+                break;
+            case "5":
+                System.out.println("Neni vsechno zlato, co se trpyti.");
+                break;
+            default: // it is optional
+                System.out.println("Konec");
+                break;
+        }
+    }
+
+}
+
+
+
